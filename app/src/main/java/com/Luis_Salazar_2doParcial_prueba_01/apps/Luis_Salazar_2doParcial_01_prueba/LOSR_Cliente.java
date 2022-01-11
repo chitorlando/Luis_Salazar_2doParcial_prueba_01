c8b896333bbd63c29935e48c6013f02ce47f5e79
chitorlando <xitodx16@gmail.com>
2022-01-10 19:01:33 -0500

    19:50

--- app/src/main/java/com/Luis_Salazar_2doParcial_prueba_01/apps/Luis_Salazar_2doParcial_01_prueba/LOSR_Cliente.java
+++ app/src/main/java/com/Luis_Salazar_2doParcial_prueba_01/apps/Luis_Salazar_2doParcial_01_prueba/LOSR_Cliente.java
@@ -1,4 +1,57 @@
 package com.Luis_Salazar_2doParcial_prueba_01.apps.Luis_Salazar_2doParcial_01_prueba;
 
 public class LOSR_Cliente {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public LOSR_Cliente() {
+    }
+
+    public LOSR_Cliente(int codigo, String nombre, String apellido, String correo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public LOSR_Cliente(String nombre, String apellido, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
 }
